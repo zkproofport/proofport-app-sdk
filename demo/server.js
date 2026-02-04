c74464e4d6f3d42301358b3136b3b9202ca5844b
jaehyuk.hyun <hyuki0130@gmail.com>
2026-02-04 15:19:35 +0900

    add OpenGraph and Twitter Card meta tags to demo pages with SVG OG image

--- demo/server.js
+++ demo/server.js
@@ -103,6 +103,20 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(html);
     });
+  } else if (req.method === 'GET' && pathname === '/og-image.svg') {
+    const filePath = path.join(__dirname, 'og-image.svg');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('OG image not found');
+        return;
+      }
+      res.writeHead(200, {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'public, max-age=86400',
+      });
+      res.end(data);
+    });
   } else if (req.method === 'GET' && pathname.startsWith('/dist/')) {
     const filePath = path.join(__dirname, '..', pathname);
     const ext = path.extname(filePath);
